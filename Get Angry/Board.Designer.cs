c3d97466162d61b1bead3c300d477aaaaad18c12
Konstantin Radke <kon.radke15@gmail.com>
2021-12-30 23:58:38 +0100

    Add board and pixel coordinates for fields

--- Get Angry/Board.Designer.cs
+++ Get Angry/Board.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Collections.Generic;
+
 namespace Get_Angry
 {
     partial class Board
@@ -29,13 +31,85 @@ namespace Get_Angry
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Board));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button_close = new System.Windows.Forms.Button();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_reset = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(768, 768);
+            this.panel1.TabIndex = 0;
+            // 
+            // button_close
+            // 
+            this.button_close.Location = new System.Drawing.Point(781, 12);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(75, 23);
+            this.button_close.TabIndex = 1;
+            this.button_close.Text = "Close";
+            this.button_close.UseVisualStyleBackColor = true;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(781, 41);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 2;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            // 
+            // button_reset
+            // 
+            this.button_reset.Location = new System.Drawing.Point(781, 70);
+            this.button_reset.Name = "button_reset";
+            this.button_reset.Size = new System.Drawing.Size(75, 23);
+            this.button_reset.TabIndex = 3;
+            this.button_reset.Text = "Reset";
+            this.button_reset.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(320, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Board
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(868, 768);
+            this.Controls.Add(this.button_reset);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.button_close);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Board";
+            this.Text = "MDGA";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_close;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_reset;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Get Angry/Board.cs
+++ Get Angry/Board.cs
@@ -12,9 +12,94 @@ namespace Get_Angry
 {
     public partial class Board : Form
     {
+        public List<Field> fields = new List<Field>()
+        {
+            // Start yellow
+            FieldFactory.from_factor(1, 5),
+            FieldFactory.from_factor(2, 5),
+            FieldFactory.from_factor(3, 5),
+            FieldFactory.from_factor(4, 5),
+            FieldFactory.from_factor(5, 5),
+            FieldFactory.from_factor(5, 4),
+            FieldFactory.from_factor(5, 3),
+            FieldFactory.from_factor(5, 2),
+            FieldFactory.from_factor(5, 1),
+            FieldFactory.from_factor(6, 1),
+            // Start green
+            FieldFactory.from_factor(7, 1),
+            FieldFactory.from_factor(7, 2),
+            FieldFactory.from_factor(7, 3),
+            FieldFactory.from_factor(7, 4),
+            FieldFactory.from_factor(7, 5),
+            FieldFactory.from_factor(8, 5),
+            FieldFactory.from_factor(9, 5),
+            FieldFactory.from_factor(10, 5),
+            FieldFactory.from_factor(11, 5),
+            FieldFactory.from_factor(11, 6),
+            // Start red
+            FieldFactory.from_factor(11, 7),
+            FieldFactory.from_factor(10, 7),
+            FieldFactory.from_factor(9, 7),
+            FieldFactory.from_factor(8, 7),
+            FieldFactory.from_factor(7, 7),
+            FieldFactory.from_factor(7, 8),
+            FieldFactory.from_factor(7, 9),
+            FieldFactory.from_factor(7, 10),
+            FieldFactory.from_factor(7, 11),
+            FieldFactory.from_factor(6, 11),
+            // Start black
+            FieldFactory.from_factor(5, 11),
+            FieldFactory.from_factor(5, 10),
+            FieldFactory.from_factor(5, 9),
+            FieldFactory.from_factor(5, 8),
+            FieldFactory.from_factor(5, 7),
+            FieldFactory.from_factor(4, 7),
+            FieldFactory.from_factor(3, 7),
+            FieldFactory.from_factor(2, 7),
+            FieldFactory.from_factor(1, 7),
+            FieldFactory.from_factor(1, 6)
+        };
+
         public Board()
         {
             InitializeComponent();
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+
+    /// <summary>
+    /// Resembles a field on the game board, used in class <see cref="Board"/>
+    /// </summary>
+    public class Field
+    {
+        private const int OFFSET = 64;
+
+        public int x;
+        public int y;
+
+        /// <summary>
+        /// Creates a new field based on x and y pixel coordinate
+        /// </summary>
+        /// <param name="coord_x">x coordinate</param>
+        /// <param name="coord_y">y coordinate</param>
+        public Field(int coord_x, int coord_y)
+        {
+            x = coord_x;
+            y = coord_y;
+        }
+    }
+
+    public static class FieldFactory
+    {
+        private const int OFFSET = 64;
+
+        public static Field from_factor(int factor_x, int factor_y)
+        {
+            return new Field(factor_x * OFFSET, factor_y * OFFSET);
         }
     }
 }
